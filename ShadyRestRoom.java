6f32521877373cd7460b8fed8a3b4882ecabf4ef
Gh-code-20 <53709080+Gh-code-20@users.noreply.github.com>
2020-02-18 14:38:40 -0500

    Update ShadyRestRoom.java

--- ShadyRestRoom.java
+++ ShadyRestRoom.java
@@ -3,12 +3,12 @@ public class ShadyRestRoom
 {
    public static void main (String[] args)
    {
-      int selection;
+      int choose;
       int price;
       String result;
       final int Queen = 1, King = 2, KingAndPullout = 3;
       final int Queen_PR = 125, King_PR = 165,
-         KingAndPullout_PR = 139;
+                KingAndPullout_PR = 139;
       final String Queen_R = "Queen ",
                    King_R = "King ",
                    KingAndPullout_R = "King and Pullout",
@@ -20,19 +20,19 @@ public class ShadyRestRoom
       System.out.println("(3) "+ KingAndPullout_R);
       System.out.print("Enter Selection (1, 2, or 3)>> ");
       selection = input.nextInt();
-      if(selection == Queen)
+      if(choose == Queen)
       {
          result = Queen_R;
          price = Queen_PR;
       }
       else
-         if(selection == King)
+         if(choose == King)
          {
             result = King_R;
             price = King_PR;
          }
          else
-            if (selection == KingAndPullout)
+            if (choose == KingAndPullout)
             {
                 result = KingAndPullout_R;
                 price = KingAndPullout_PR;
