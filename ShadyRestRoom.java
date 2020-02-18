922bfa8f4143f7f4fc2451017349a27ae65c952f
Gh-code-20 <53709080+Gh-code-20@users.noreply.github.com>
2020-02-18 14:29:38 -0500

    Update ShadyRestRoom.java

--- ShadyRestRoom.java
+++ ShadyRestRoom.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class ShadyRestRoom
 {
-   public static void main (String args[])
+   public static void main (String[] args)
    {
       int selection;
       int price;
