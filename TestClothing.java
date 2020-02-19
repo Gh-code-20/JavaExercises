c152747ec7632c8ef41f7028bd1061e45870cbab
Ghadir-code-20 <53709080+Gh-code-20@users.noreply.github.com>
2020-02-19 17:01:35 -0500

    Update TestClothing.java

--- TestClothing.java
+++ TestClothing.java
@@ -12,3 +12,12 @@ public class TestClothing
 
     }
 }
+
+/*Create a Shopping class that includes data fields for the name of a clothing product, the cost of the product,  
+the quantity of the product (how many of each), and the sales tax.  
+The class includes methods to get each field.
+Create a default constructor that automatically sets the name of the clothing to shirts, 
+the cost to 15.00, the quantity to 5, and the sales tax to .06. Save the file as Clothing.java. 
+Create an application that demonstrates each method works correctly 
+and calculate the total cost of what the customer will owe and display it, 
+along with displaying all of the data from each of the variables. Upload the .java file to this assignment. */ 
