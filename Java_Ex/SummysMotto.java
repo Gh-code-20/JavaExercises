e407eac6b42cf48de08549bfb39ffb957542a15b
Ghadir-code-20 <53709080+Gh-code-20@users.noreply.github.com>
2021-01-03 14:36:48 -0500

    Delete SummysMotto.java

--- Java_Ex/SummysMotto.java
+++
@@ -1,9 +0,0 @@
-public class SummysMotto
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Sammy's makes it fun in the sun.");
-    }
-}
-
-//create program that displays Sammy's makes it fun in the sun.
