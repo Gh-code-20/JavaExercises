3dad1b84baa0d166f813163be939276b067cde61
Gh-code-20 <53709080+Gh-code-20@users.noreply.github.com>
2022-06-30 23:45:01 -0400

    Create CountWords.java

---
+++ Java_Ex/CountWords.java
@@ -0,0 +1,39 @@
+//Ghadir Alfadhl
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class CountWords {
+    public static void main(String[] args) {
+        String name;
+        char c;
+        int stringLength;
+        int i;
+        int wordCount = 0;
+        int lCount = 0;
+        int lastPos;
+        name = JOptionPane.showInputDialog(null, "Enter a sentence or any words");
+        stringLength = name.length();
+        lastPos = stringLength;
+        for (i = 0; i < stringLength; i++) {
+            c = name.charAt(i);
+            if (Character.isLetterOrDigit(c))
+                lCount = lCount + 1;
+            if (lCount > 0 && wordCount == 0)
+                wordCount = 1;
+            if (lCount > 0)
+                if (c == ' ' || c == '.' || c == '-' || c == '!' || c == ',' || c == '?') {
+                    lCount = 0;
+                    wordCount = wordCount + 1;
+                    if (lastPos == i + 1)
+                        if (c == ' ' || c == '.' || c == '-' || c == '!' || c == ',' || c == '?')
+                            wordCount = wordCount - 1;
+
+                }
+        }
+        if (wordCount > 0)
+            System.out.println("There are  " + wordCount + " words in the string");
+        else
+            System.out.println("You have typed no words");
+
+    }
+}
